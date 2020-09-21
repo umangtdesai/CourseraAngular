209964552b725bb4941ae0e9c02211d4e973f91b
umangtdesai <utdesai@bu.edu>
2020-09-21 23:43:20 +0530

    mod1_soln completed

--- mod1_soln/app.js
+++ mod1_soln/app.js
@@ -1,5 +1,5 @@
 (function () {
-    'use strict'
+    'use strict';
 
     // step 7
     angular.module('LunchCheck', [])
@@ -9,11 +9,31 @@
     LunchCheckController.$inject = ['$scope'];
 
     function LunchCheckController($scope) {
-        $scope.placeholder = "List comma separated dishes you usually have for lunch";
+        $scope.placeholder = "List comma separated dishes you usually have for lunch"
+        $scope.message = "";
+
         $scope.getMessage = function () {
-            // implementation here
             return "blah"
-        }
+        };
+
+        $scope.buttonClicked = function() {
+
+            var ogPlaceholder = "List comma separated dishes you usually have for lunch";
+
+            var items = $scope.placeholder.split(',');
+            if ($scope.placeholder == "" || $scope.placeholder == ogPlaceholder) {
+                $scope.message= "Please enter data first"
+            }
+            else if (items.length <= 2) {
+                $scope.message = "Too Little!";
+            }
+            else if (items.length <= 3) {
+                $scope.message = "Enjoy!";
+            }
+            else {
+                $scope.message = "Too much!";
+            }
+        };
     }
 
 
